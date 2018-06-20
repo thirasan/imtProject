77d0de8b51e725b888e7a1d7a030b21960298f7d
Thirasan <thirasan@salsa-26-174.priv.enst-bretagne.fr>
2018-06-20 11:50:04 +0200

    testCommit

--- statisticPlot.py
+++ statisticPlot.py
@@ -10,12 +10,16 @@ fig, ax = plt.subplots(1, 1)
 
 mean, var, skew, kurt = norm.stats(moments='mvsk')
 
-x = df.Y.values
+x = np.linspace(norm.ppf(0.01),
+                norm.ppf(0.99), 100)
 ax.plot(x, norm.pdf(x),
        'r-', lw=5, alpha=0.6, label='norm pdf')
 
 rv = norm()
 ax.plot(x, rv.pdf(x), 'k-', lw=2, label='frozen pdf')
+
+vals = norm.ppf([0.001, 0.5, 0.999])
+np.allclose([0.001, 0.5, 0.999], norm.cdf(vals))
 
 r = norm.rvs(size=300)
 
