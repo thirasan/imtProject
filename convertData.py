b3c22785f06d96b6a41dba3fa9286258cabdae87
Thirasan <thirasan@salsa-26-174.priv.enst-bretagne.fr>
2018-06-20 11:41:33 +0200

    initial_version_convert

---
+++ convertData.py
@@ -0,0 +1,26 @@
+import csv
+
+with open('003162-av.pro2.csv', newline='') as csvfile:
+    spamReader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    count = 1
+
+    line = iter(spamReader)
+    for row in line:
+        parameter = row[0].split('NaN')
+
+        x = parameter[0].split(';')
+        y = parameter[1].split(';')
+        intensity = parameter[2].split(';')
+
+        x = [item for item in x if item not in ' ']
+        y = [item for item in y if item not in ' ']
+        intensity = [item for item in intensity if item not in ' ']
+
+        with open('profile_no1_data/profile' + str(count) + '.csv', 'w', newline='') as csvfile:
+            fieldnames = ['X', 'Y', 'Intensity']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            for i, j, k in zip(x, y, intensity):
+                writer.writerow({'X': '' + i, 'Y': '' + j, 'Intensity': '' + k})
+        count += 1
