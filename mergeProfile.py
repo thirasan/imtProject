caa868dfc2dfb221c9767d6af959aa0cc123f3fe
Thirasan <thirasan@salsa-26-52.priv.enst-bretagne.fr>
2018-07-05 14:05:00 +0200

    Identified3rdEdition

--- mergeProfile.py
+++ mergeProfile.py
@@ -13,7 +13,7 @@ pa2 = list()
 pa3 = list()
 pa4 = list()
 
-for i in range(1, 12884):
+for i in range(4500, 4800):
     try:
         df = pd.read_csv('profile_no1_data/profile' + str(i) + '.csv')
         # saturation variable contain only 254 value
@@ -28,6 +28,9 @@ for i in range(1, 12884):
                 localMean = mean([df.loc[temp].Y, df.loc[temp + 1].Y, df.loc[temp + 2].Y, df.loc[temp + 3].Y,
                                   df.loc[temp + 4].Y])
                 while localMean + 1.5 > df.loc[temp].Y > localMean - 1.5:
+                    if temp < 10:
+                        # temp = saturation.index[0 + pointer]
+                        break
                     temp -= 1
                 index_of_p2 = temp
                 p2 = df.loc[index_of_p2]
@@ -45,6 +48,10 @@ for i in range(1, 12884):
                 localMean = mean([df.loc[temp].Y, df.loc[temp - 1].Y, df.loc[temp - 2].Y, df.loc[temp - 3].Y,
                                   df.loc[temp - 4].Y])
                 while localMean + 1.5 > df.loc[temp].Y > localMean - 1.5:
+                    if temp > len(df.index) - 10:
+                        temp -= 1
+                        # temp = saturation.index[-(1 + pointer)]
+                        break
                     temp += 1
                 index_of_p3 = temp
                 p3 = df.loc[index_of_p3]
@@ -58,13 +65,17 @@ for i in range(1, 12884):
         while True:
             localMean = mean([df.loc[index_of_p2].Y, df.loc[index_of_p2 - 1].Y, df.loc[index_of_p2 - 2].Y,
                               df.loc[index_of_p2 - 3].Y, df.loc[index_of_p2 - 4].Y, df.loc[index_of_p2 - 5].Y,
-                             df.loc[index_of_p2 - 6].Y, df.loc[index_of_p2 - 7].Y,df.loc[index_of_p2 - 8].Y,
-                             df.loc[index_of_p2 - 9].Y])
+                              df.loc[index_of_p2 - 6].Y, df.loc[index_of_p2 - 7].Y, df.loc[index_of_p2 - 8].Y,
+                              df.loc[index_of_p2 - 9].Y])
+
+            p1 = df.loc[index_of_p2]
             if localMean + 0.1 > df.loc[index_of_p2].Y > localMean - 0.1:
-                p1 = df.loc[index_of_p2]
+                if p2.X - p1.X < 4.5:
+                    index_of_p2 -= 1
+                    continue
                 inp1 = index_of_p2
                 break
-            elif index_of_p2 <= 5:
+            elif index_of_p2 <= 11:
                 break
             else:
                 index_of_p2 -= 1
@@ -74,30 +85,33 @@ for i in range(1, 12884):
         while True:
             localMean = mean([df.loc[index_of_p3].Y, df.loc[index_of_p3 + 1].Y, df.loc[index_of_p3 + 2].Y,
                               df.loc[index_of_p3 + 3].Y, df.loc[index_of_p3 + 4].Y, df.loc[index_of_p3 + 5].Y,
-                             df.loc[index_of_p3 + 6].Y, df.loc[index_of_p3 + 7].Y,
-                             df.loc[index_of_p3 + 8].Y, df.loc[index_of_p3 + 9].Y])
+                              df.loc[index_of_p3 + 6].Y, df.loc[index_of_p3 + 7].Y,
+                              df.loc[index_of_p3 + 8].Y, df.loc[index_of_p3 + 9].Y])
+
+            p4 = df.loc[index_of_p3]
             if localMean + 0.1 > df.loc[index_of_p3].Y > localMean - 0.1:
-                p4 = df.loc[index_of_p3]
+                if p4.X - p3.X < 4.5:
+                    index_of_p3 += 1
+                    continue
                 inp4 = index_of_p3
                 break
-            elif index_of_p3 >= len(df.index) - 5:
+            elif index_of_p3 >= len(df.index) - 11:
                 break
             else:
                 index_of_p3 += 1
                 continue
 
         # add axis of p1-p4 to variable
-        # pa1.append(p1.X)
-        # pa2.append(p2.X)
-        # pa3.append(p3.X)
-        # pa4.append(p4.X)
+        pa1.append(p1.X)
+        pa2.append(p2.X)
+        pa3.append(p3.X)
+        pa4.append(p4.X)
 
         # add index of p1-p4 to variable
-        pa1.append(inp1)
-        pa2.append(inp2)
-        pa3.append(inp3)
-        pa4.append(inp4)
-
+        # pa1.append(inp1)
+        # pa2.append(inp2)
+        # pa3.append(inp3)
+        # pa4.append(inp4)
     except:
         print("Error with file" + str(i))
 
